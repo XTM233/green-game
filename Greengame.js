942f79b524b61e2850465af18ea156f861761a18
Mingxin.Shen <mingxin.shen20@imperial.ac.uk>
2021-07-24 14:02:47 +0800

    note-sync syncing

--- Greengame.js
+++ Greengame.js
@@ -65,8 +65,7 @@ function Land(type = "forest", position) {
 Land.prototype = {
         constructor: Land,
         applyTechnology(tech): function { // drag event 玩家拖拽图标到可以apply科技的格子触发
-            if (tech = "dock")
-                and(position not in coast) {
+            if (tech = "dock") and(position not in coast) {
                     console.error(tech);
                 } else {
                     this.type = tech;
